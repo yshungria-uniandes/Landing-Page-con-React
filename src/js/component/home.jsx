1d46b158e5337e634e2741c4c8a54162f96c0abb
yshungria-uniandes <ys.hungria@uniandes.edu.co>
2024-07-31 20:19:12 -0500

    Cards Avanced Landing Page React

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,20 @@
 import React from "react";
-
+import Card from "./Card";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container">
+			<h1 className="text-center mt-5">Landing Page React</h1>
+			<div className="cards d-flex justify-content-center gap-3">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
+			
 		</div>
 	);
 };
