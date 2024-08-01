1290c011bb178a3e7167e6b031ff69ec3efc17ef
LuisRe135 <64992020+LuisRe135@users.noreply.github.com>
2024-08-01 01:44:05 +0000

    first footer

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="bg-dark text-light">
+            Copyright © {new Date().getFullYear()}
+        </div>
+    )
+}
+
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "./Card";
+import Footer from "./Footer";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
@@ -14,6 +15,7 @@ const Home = () => {
 				<Card />
 				<Card />
 			</div>
+			<Footer />
 			
 		</div>
 	);
